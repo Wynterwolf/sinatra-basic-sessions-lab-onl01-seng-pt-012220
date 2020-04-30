383aab96071c7a7a6795f36dab0316fbf099eb3c
Samantha Jordan <shinyguns@gmail.com>
2020-04-30 08:49:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :sesssion_secret, "secret"
+  end
+  
+  get '/' do
+    erb :index
+  end
+  
+  post '/checkout' do
+  
+  end
 end
